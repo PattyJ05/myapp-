78b46abfb9ab94493909b26e133fdf3353574a0f
franc <franc@PATTY>
2025-03-01 15:30:46 -0600

    Agregar archivos de proyecto.

---
+++ myapp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace myapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "myapp" };
+        }
+    }
+}
---
+++ myapp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace myapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
